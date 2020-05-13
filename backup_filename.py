3e203e41d7333254b97ef6f42a5c6a70c8d4192b
joyceChu2020 <65305235+joyceChu2020@users.noreply.github.com>
2020-05-13 15:27:22 -0400

    Update backup_filename.py

--- backup_filename.py
+++ backup_filename.py
@@ -7,7 +7,7 @@ outputpath = 'structure would be concatenated to this outputpath'
 for dirpath, dirnames, filenames in os.walk(inputpath):
     for filen in filenames:
         if filen.endswith('.raw'):
-            structure = os.path.join(outputpath, dirpath[len(inputpath):])
+            structure = os.path.join(outputpath, dirpath[len(inputpath):]) #if the join function messes things up then simply use'+'
             os.makedirs(structure, exist_ok=True)
             shutil.copy(os.path.join(dirpath, filen), structure)
     print('job is done!')
