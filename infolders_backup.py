e1a9a3346393fff4b2b5a139cda66ee3e6f4812f
joyceChu2020 <65305235+joyceChu2020@users.noreply.github.com>
2020-05-13 14:27:28 -0400

    Create infolders_backup.py

---
+++ infolders_backup.py
@@ -0,0 +1,13 @@
+inputpath='everything before the structure comes in'
+outputpath='structure comes after'
+
+for path, dirs, files in os.walk(os.getcwd()):
+    for dir in dirs:
+        if dir=='this' or dir=='folder':
+            for a, b, c in os.walk(os.path.join(path,dir)):
+                for cc in c:
+                    frompath=os.path.join(a,cc)
+                    structure=os.path.join(outputpath, a[len(inputpath):])
+                    os.makedirs(structure, exist_ok=True)
+                    shutil.copy(frompath, structure)
+    print('Finished')                    
