913a414861186a20a67eceb390e3272e1a4aa27f
joyceChu2020 <65305235+joyceChu2020@users.noreply.github.com>
2020-05-13 15:02:15 -0400

    Update infolders_backup.py

--- infolders_backup.py
+++ infolders_backup.py
@@ -7,7 +7,7 @@ outputpath='structure comes after'
 for path, dirs, files in os.walk(os.getcwd()):
     for dir in dirs:
         if dir=='this' or dir=='folder':
-            for a, b, c in os.walk(os.path.join(path,dir)):
+            for a, _, c in os.walk(os.path.join(path,dir)):
                 for cc in c:
                     frompath=os.path.join(a,cc)
                     structure=os.path.join(outputpath, a[len(inputpath):])
